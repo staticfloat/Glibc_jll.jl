006a0f2ef172afd3d9703f2fedbee550df575a4f
Elliot Saba <staticfloat@gmail.com>
2024-06-19 18:41:44 +0000

    Glibc v2.17.1

--- src/Glibc_jll.jl
+++ src/Glibc_jll.jl
@@ -1,9 +1,5 @@
-# Use baremodule to shave off a few KB from the serialized `.ji` file
-baremodule Glibc_jll
-using Base
-using Base: UUID
-import JLLWrappers
+module Glibc_jll
+using LazyJLLWrappers
+@generate_jll_from_toml()
+end # module Glibc_jll
 
-JLLWrappers.@generate_main_file_header("Glibc")
-JLLWrappers.@generate_main_file("Glibc", UUID("452aa2e7-e185-58db-8ff9-d3c1fa4bc997"))
-end  # module Glibc_jll
